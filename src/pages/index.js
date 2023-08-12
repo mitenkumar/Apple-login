07d380999bc12915e8531ca0596b25b8bf300b4f
mitenkumar <mitup7869@gmail.com>
2023-08-12 12:33:00 +0530

    session print

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,26 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const handleAppleLogin = async () => {
   try {
     const result = await signIn("apple", {
-      callbackUrl:"https://apple-login-zeta.vercel.app/",
+      callbackUrl: "https://apple-login-zeta.vercel.app/",
     });
     console.log("SignIn Result:", result);
   } catch (error) {
     console.error("Error during Apple Sign-In:", error);
   }
 };
+
 export default function Home() {
+  const { data: session } = useSession();
+
+  if (session) {
+    console.log(session);
+  }
   return (
     <>
       <button onClick={handleAppleLogin}>sign in with apple </button>
