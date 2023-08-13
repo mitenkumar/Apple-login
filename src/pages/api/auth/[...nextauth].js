2d2073439e9ef218c6e73fe3802fa6cf8d3cb94b
mitenkumar <mitup7869@gmail.com>
2023-08-13 20:01:46 +0530

    nextauth rollback

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,44 +1,25 @@
-import { signIn } from "next-auth/react";
+import NextAuth from "next-auth";
+
 import AppleProvider from "next-auth/providers/apple";
-import NextAuth from "next-auth/next";
-const handleAppleSignIn = async (e) => {
-  e.preventDefault();
-  signIn("apple");
-};
 
 export default NextAuth({
   providers: [
     AppleProvider({
       clientId: process.env.APPLE_CLIENT_ID,
+
       clientSecret: process.env.APPLE_CLIENT_SECRET,
+
+      scope: "name email",
+
+      profile: (profile) => {
+        return {
+          id: profile.sub,
+
+          name: profile.name,
+
+          email: profile.email,
+        };
+      },
     }),
   ],
-  cookies: {
-    pkceCodeVerifier: {
-      name: "next-auth.pkce.code_verifier",
-      options: {
-        httpOnly: true,
-        sameSite: "none",
-        path: "/",
-        secure: true,
-      },
-    },
-  },
-  pages: {
-      signIn: '/register'
-  },
-  callbacks: {
-      async signIn(user, account, profile) {
-          console.log(user);
-          return true;
-      },
-      async redirect(url, baseUrl) {
-          return `${process.env.NEXTAUTH_URL}/register`;
-      },
-  },
-  callbackUrl: `${process.env.NEXTAUTH_URL}/accredited-investor`,
-
-  secret: process.env.NEXTAUTH_SECRET,
 });
-
-export { handleAppleSignIn };
