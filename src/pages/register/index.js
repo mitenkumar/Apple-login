3628f15cae2d41dc5d065d310fa753846cdcf0de
mitenkumar <mitup7869@gmail.com>
2023-08-13 20:05:15 +0530

    indexjs

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,13 +1,12 @@
 import { signOut, useSession } from "next-auth/react";
-import { handleAppleSignIn } from "../api/auth/[...nextauth]";
 
-// const handleAppleLogin = async () => {
-//   try {
-//     await signIn("apple");
-//   } catch (error) {
-//     console.error("Error during Apple Sign-In:", error);
-//   }
-// };
+const handleAppleLogin = async () => {
+  try {
+    await signIn("apple");
+  } catch (error) {
+    console.error("Error during Apple Sign-In:", error);
+  }
+};
 
 export default function Home() {
   const { data: session } = useSession();
@@ -16,7 +15,7 @@ export default function Home() {
   return (
     <div>
       {!session ? (
-        <button onClick={(e)=>handleAppleSignIn(e)}>Sign in with Apple</button>
+        <button onClick={handleAppleLogin}>Sign in with Apple</button>
       ) : (
         <div>
           <p>Email: {session.user.email}</p>
