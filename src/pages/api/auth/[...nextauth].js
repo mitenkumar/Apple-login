1d1a171850f8d397debb55494fdea43dec772956
mitenkumar <mitup7869@gmail.com>
2023-08-13 20:08:48 +0530

    update and put sign in functonality

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -6,17 +6,12 @@ export default NextAuth({
   providers: [
     AppleProvider({
       clientId: process.env.APPLE_CLIENT_ID,
-
       clientSecret: process.env.APPLE_CLIENT_SECRET,
-
       scope: "name email",
-
       profile: (profile) => {
         return {
           id: profile.sub,
-
           name: profile.name,
-
           email: profile.email,
         };
       },
--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,4 +1,4 @@
-import { signOut, useSession } from "next-auth/react";
+import { signIn,signOut, useSession } from "next-auth/react";
 
 const handleAppleLogin = async () => {
   try {
